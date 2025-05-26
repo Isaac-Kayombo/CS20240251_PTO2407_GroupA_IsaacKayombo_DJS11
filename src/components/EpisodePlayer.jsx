04b81c013d9161c6f06937cedfc79d2eec976afc
IsaacK47 <isaackayombo41@gmail.com>
2025-05-26 20:37:27 +0200

    Creating episode player component

---
+++ src/components/EpisodePlayer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function EpisodePlayer() {
+
+
+    return (
+        <div className='episode-player'>
+            <button className='play-button'>Play</button>
+            <button>❤</button>
+        </div>
+    )
+}
+
+export default EpisodePlayer;
