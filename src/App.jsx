ee97b26275b03b92861d110751b1fc4f9213a84e
IsaacK47 <isaackayombo41@gmail.com>
2025-05-26 10:38:21 +0200

    Configuring react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
 
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
