12d49f4a1152a70bb88d38dad232741e90de19e8
IsaacK47 <isaackayombo41@gmail.com>
2025-05-26 20:04:58 +0200

    Rendering podcast page

--- src/pages/PodcastDetail.jsx
+++ src/pages/PodcastDetail.jsx
@@ -1,8 +1,42 @@
-
+import React from 'react'
+import { useParams } from 'react-router-dom';
 
 
 function PodcastDetail() {
+    const { id } = useParams();
+    const [podcast, setPodcast] = React.useState(null);
 
+    React.useEffect(() => {
+        fetch(`https://podcast-api.netlify.app/id/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setPodcast(data);
+            })
+    }, [id]);
+
+    return (
+        <div className='podcast-detail'>
+            <h1>{podcast.title}</h1>
+            <img src={podcast.image} alt={podcast.title}/>
+            <p>{podcast.description}</p>
+            <h2>Seasons</h2>
+            <ul>
+                {podcast.seasons.map((season, index) => (
+                    <li key={index}>
+                        <h3>{season.title}</h3>
+                        <h4>{season.episodes.length} Episodes</h4>
+                        <ul>
+                            {season.episodes.map((episode, index2) => (
+                                <li key={index2}>
+                                    <strong>{episode.title}</strong>
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default PodcastDetail;
