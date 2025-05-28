1f9b237fdb9f21f039b26c90dea1b43702245027
IsaacK47 <isaackayombo41@gmail.com>
2025-05-28 18:40:44 +0200

    Adding comments to episode player component

--- src/components/EpisodePlayer.jsx
+++ src/components/EpisodePlayer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+// DEFINING EPISODE PLAYER COMPONENT
+
 function EpisodePlayer({ episode, showTitle, seasonTitle }) {
+    // STATE HOOKS FOR AUDIO CONTROL, TRACK PLAYING STATE, IF EPISODE HAS BEEN FAVORITED STATE
     const audioRef = React.useRef(null);
     const [isPlaying, setIsPlaying] = React.useState(false);
     const [isFavorite, setIsFavorite] = React.useState(false);
 
+    // TOGGELS THE PLAY/PAUSE AUDIO ELEMENT
     const togglePlay = () => {
         const audio =audioRef.current;
         if (!audio) return;
@@ -16,9 +20,11 @@ function EpisodePlayer({ episode, showTitle, seasonTitle }) {
         }
     };
 
+    // ADDS EPISODE TO LOCAL STORAGE FAVORITES IF NOT ADDED ALREADY
     const handleFavorite = () => {
         const existingFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
+        // creates new favorite with metadata
         const newFavorite = {
             ...episode,
             showTitle,
@@ -26,10 +32,12 @@ function EpisodePlayer({ episode, showTitle, seasonTitle }) {
             addedAt: new Date().toISOString()
         };
 
+        // if episode is already in favorites (based on title and showTitle)
         const isDuplicate = existingFavorites.some(
             (ep) => ep.title === episode.title && ep.showTitle === showTitle
         );
 
+        // adds it and update state and localStorage (if not favorite already)
         if (!isDuplicate) {
             existingFavorites.push(newFavorite);
             localStorage.setItem("favorites", JSON.stringify(existingFavorites));
@@ -37,6 +45,7 @@ function EpisodePlayer({ episode, showTitle, seasonTitle }) {
         }
     };
 
+    // attach audio event listeners and clean them up when component unmounts
     React.useEffect(() => {
         const audio = audioRef.current;
         if (!audio) return;
@@ -45,10 +54,12 @@ function EpisodePlayer({ episode, showTitle, seasonTitle }) {
         const handlePlay = () => setIsPlaying(true);
         const handlePause = () => setIsPlaying(false);
 
+        // Event listeners to audio element
         audio.addEventListener("ended", handleEnded);
         audio.addEventListener("play", handlePlay);
         audio.addEventListener("pause", handlePause);
 
+        // Removes event listeners on unmount
         return () => {
             audio.removeEventListener("ended", handleEnded);
             audio.removeEventListener("play", handlePlay);
@@ -56,6 +67,7 @@ function EpisodePlayer({ episode, showTitle, seasonTitle }) {
         };
     }, []);
 
+    // RENDERS PLAY/PAUSE AND FAVORITE BUTTONS
     return (
         <div className='episode-player'>
             <button className='play-button' onClick={togglePlay}>
